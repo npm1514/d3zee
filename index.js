0c88d4284aeaa7ba6a4b4f3a556d5aefd2a9ab11
Nick Marucci <npm1514@gmail.com>
2017-01-23 20:33:50 -0700

    trying

---
+++ index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+
+var app = express();
+
+app.use(express.static(__dirname));
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("listening to 80 or 3000");
+});
