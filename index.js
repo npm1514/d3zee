b968a2c089af065762787acde2274fb1e19b1260
NIck Marucci <npm1514@gmail.com>
2024-06-07 20:31:38 -0600

    add cors

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
+var cors = require('cors');
+
 var app = express();
 app.use(express.static(__dirname));
+app.use(cors())
 
 app.listen(process.env.PORT || 3000, function(){
   console.log("listening to 80 or 3000");
