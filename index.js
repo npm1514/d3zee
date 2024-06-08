fe8a6edb663175dbc4def24ed29a7dee78be9b80
NIck Marucci <npm1514@gmail.com>
2024-06-07 20:58:12 -0600

    make it work

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var express = require('express');
-var cors = require('cors');
+var cors = require('cors')
 
 var app = express();
+
+app.use(cors());
 app.use(express.static(__dirname));
-app.use(cors())
 
 app.listen(process.env.PORT || 3000, function(){
   console.log("listening to 80 or 3000");
